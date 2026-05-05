dacb84750a0ba7df7983ca10bed184cd5ed7246c
famalkhadija <famalkhadija@gmail.com>
2026-05-05 20:32:31 +0500

    finalTask

--- Blood-Donation-Management/frontend/src/pages/Login.jsx
+++ Blood-Donation-Management/frontend/src/pages/Login.jsx
@@ -37,7 +37,7 @@ export default function Login() {
     <div className="flex">
       <div className="left hidden md:block w-[25vw] bg-gray-100 h-screen">
         <div className="flex justify-center items-center mt-30 gap-2">
-          <img className="w-18" src={logo} alt="logo" />
+          <img className="w-18" src="/blood-icon.png" />{" "}
           <h1 className="font-bold text-2xl text-purple-950 pt-5">BDMS</h1>
         </div>
         <p className="text-center mt-10 text-purple-950 px-5 xl:px-20">
